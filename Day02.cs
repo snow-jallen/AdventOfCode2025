29e88430ef6a9f5194113acd2d2a2dfac4de4417
Jonathan Allen <jallen@snow.edu>
2025-12-02 20:40:58 -0700

    day2 part 1

--- Day02.cs
+++ Day02.cs
@@ -1,6 +1,27 @@
 string path = args.FirstOrDefault() == "real" ? "real.txt" : "sample.txt";
 var lines = File.ReadAllLines(path);
-foreach (var range in string.Join("\r\n", lines).Split(',', StringSplitOptions.RemoveEmptyEntries))
+
+List<long> allInvalid = new();
+foreach (var range in lines.SelectMany(l => l.Split(',', StringSplitOptions.RemoveEmptyEntries)))
 {
-    Console.WriteLine(range);
+    var parts = range.Split('-');
+    var min = long.Parse(parts[0]);
+    var max = long.Parse(parts[1]);
+    for(long n = min; n <= max; n++)
+    {
+        if (IsInvalid(n))
+            allInvalid.Add(n);
+    }
+}
+Console.WriteLine(allInvalid.Sum());
+
+bool IsInvalid(long number)
+{
+    var numberStr = number.ToString();
+    if (numberStr.Length < 2 || numberStr.Length % 2 != 0)
+        return false;
+    
+    var firstHalf = numberStr.Substring(0, numberStr.Length / 2);
+    var secondHalf = numberStr.Substring(numberStr.Length / 2);
+    return firstHalf.SequenceEqual(secondHalf);
 }
