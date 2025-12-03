edb78b93bc51d5d25aa70fed3779dd05bbdcc361
Jonathan Allen <jallen@snow.edu>
2025-12-02 23:11:17 -0700

    Day 3 part 2

--- Day03.cs
+++ Day03.cs
@@ -1,36 +1,104 @@
 string path = args.FirstOrDefault() == "real" ? "real.txt" : "sample.txt";
 var lines = File.ReadAllLines(path);
-var joltages = new List<Joltage>();
+var batteryBanks = new List<BatteryBank>();
+var origColor = Console.ForegroundColor;
+
 foreach (var bank in lines)
 {
-    joltages.Add(new Joltage(bank));
+    batteryBanks.Add(new BatteryBank(bank));
+    Console.Write($"Processing Bank {batteryBanks.Count}: ");
+    batteryBanks.Last().Calculate();
 }
-Console.WriteLine($"Total Joltages Processed: {joltages.Sum(j => j.JoltageValue)}");
+Console.ForegroundColor = origColor;
+Console.WriteLine($"Total Joltages Processed: {batteryBanks.Sum(b => b.JoltageValue)}");
 
-public class Joltage
+
+public class BatteryBank
 {
-    public int Offset1 { get; set; }
-    public int Offset2 { get; set; }
-    public int JoltageValue { get; set; }
-    public Joltage(string bank)
+    public BatteryBank(string batteries)
     {
-
-        int maxJoltage = 0;
-        for (int i = 0; i < bank.Length - 1; i++)
+        Batteries = new Dictionary<int, (int, bool)>();
+        for (int i = 0; i < batteries.Length; i++)
         {
-            for (int j = i + 1; j < bank.Length; j++)
+            Batteries.Add(i, (int.Parse(batteries[i].ToString()), false));
+        }
+    }
+
+    public BatteryBank Copy()
+    {
+        var copy = new BatteryBank("");
+        foreach (var battery in Batteries)
+        {
+            copy.Batteries.Add(battery.Key, (battery.Value.Value, battery.Value.TurnedOn));
+        }
+        return copy;
+    }
+
+    public void Calculate()
+    {
+        TurnOn();
+
+        var origColor = Console.ForegroundColor;
+        print();
+        Console.ForegroundColor = origColor;
+    }
+
+    public int BatteriesTurnedOn => Batteries.Values.Count(b => b.TurnedOn);
+    public long JoltageValue
+    {
+        get
+        {
+            var numString = string.Join("", Batteries.Values.Where(b => b.TurnedOn).Select(b => b.Value.ToString()));
+            if (numString.Length == 0)
             {
-                int joltage = int.Parse(bank[i].ToString()) * 10 + int.Parse(bank[j].ToString());
-                if (joltage > maxJoltage)
-                {
-                    maxJoltage = joltage;
-                    Offset1 = i;
-                    Offset2 = j;
-                    JoltageValue = joltage;
-                }
+                return 0;
+            }
+            return long.Parse(numString);
+        }
+    }
+
+    public void TurnOn()
+    {
+        var positionsToCheck = Batteries.Keys.Where(k => Batteries[k].TurnedOn is false).ToList();
+        if (positionsToCheck.Count == 0 || BatteriesTurnedOn >= 12)
+        {
+            return;
+        }
+        long newJoltage = 0;
+        int indexToTurnOn = -1;
+        foreach (var pos in positionsToCheck)
+        {
+            var alternateBank = this.Copy();
+            alternateBank.Batteries[pos] = (alternateBank.Batteries[pos].Value, true);
+            if (alternateBank.JoltageValue > newJoltage)
+            {
+                newJoltage = alternateBank.JoltageValue;
+                indexToTurnOn = pos;
             }
         }
+        Batteries[indexToTurnOn] = (Batteries[indexToTurnOn].Value, true);
+        if (positionsToCheck.Count > 1)
+        {
+            TurnOn();
+        }
+    }
 
-        Console.WriteLine($"Max Joltage: {JoltageValue} (Offsets: {Offset1}, {Offset2})");
+    public Dictionary<int, (int Value, bool TurnedOn)> Batteries { get; set; }
+
+    private void print()
+    {
+        foreach (var battery in Batteries)
+        {
+            if (battery.Value.TurnedOn)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.Write(battery.Value.Value);
+        }
+        Console.WriteLine();
     }
 }
