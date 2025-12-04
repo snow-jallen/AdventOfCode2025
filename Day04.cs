5ebf02b3a9fc61645e4f7211257a3b4782baf888
Jonathan Allen <jallen@snow.edu>
2025-12-03 22:46:16 -0700

    day 4 part 2 progress

--- Day04.cs
+++ Day04.cs
@@ -1,27 +1,45 @@
 string path = args.FirstOrDefault() == "real" ? "real.txt" : "sample.txt";
 var lines = File.ReadAllLines(path);
 long movableRolls = 0;
-for(long row = 0; row < lines.Length; row++)
+int generation = 0;
+var newRollsMoved = moveRolls(ref generation);
+movableRolls += newRollsMoved;
+while(newRollsMoved > 0)
 {
-    for(long col = 0; col < lines[row].Length; col++)
-    {
-        var current = lines[(int)row][(int)col];
-        if(current == '.')
-        {
-            continue;
-        }
-        var neighbors = neighborCount(row, col);
-        if(neighbors < 4)
-        {
-            movableRolls++;
-        }
-    }
+    newRollsMoved = moveRolls(ref generation);
+    movableRolls += newRollsMoved;
 }
 Console.WriteLine($"Total Movable Rolls: {movableRolls}");
 
-long neighborCount(long row, long col)
+int moveRolls(ref int generation)
 {
-    long neighbors = 0;
+    Console.Write($"Generation {generation++}");
+
+    int movableRolls = 0;
+    for(int row = 0; row < lines.Length; row++)
+    {
+        for(int col = 0; col < lines[row].Length; col++)
+        {
+            var current = lines[row][col];
+            if(current == '.')
+            {
+                continue;
+            }
+            var neighbors = neighborCount(row, col);
+            if(neighbors < 4)
+            {
+                movableRolls++;
+                lines[row] = lines[row].Remove(col, 1).Insert(col, "@");
+            }
+        }
+    }
+    Console.WriteLine($" Moved Rolls: {movableRolls}");
+    return movableRolls;
+}
+
+int neighborCount(int row, int col)
+{
+    int neighbors = 0;
     if(getValue(row, col-1) == '@') neighbors++;
     if(getValue(row, col+1) == '@') neighbors++;
     if(getValue(row-1, col-1) == '@') neighbors++;
@@ -33,9 +51,9 @@ long neighborCount(long row, long col)
     return neighbors;
 }
 
-char getValue(long row, long col)
+char getValue(int row, int col)
 {
     if(row < 0 || row >= lines.Length) return '.';
     if(col < 0 || col >= lines[row].Length) return '.';
-    return lines[(int)row][(int)col];
+    return lines[row][col];
 }
