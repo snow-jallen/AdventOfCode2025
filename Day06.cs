989f4a9e3aafcdc7a79aca317a7731c1c2eefd52
Jonathan Allen <jallen@snow.edu>
2025-12-06 00:00:45 -0700

    day 6 part 2 progress

--- Day06.cs
+++ Day06.cs
@@ -3,27 +3,38 @@ string path = args.FirstOrDefault() == "real" ? "real.txt" : "sample.txt";
 var lines = File.ReadAllLines(path);
 var lineLen = lines[0].Length;
 var columns = new List<Column>();
+var operations = lines.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+foreach(var op in operations)
+{
+    columns.Add(new Column());
+}
+
 foreach (var line in lines.Take(lines.Length - 1))
 {
     var columnIndex = 0;
     var newValue = 0L;
     for (int i = 0; i < lineLen; i++)
     {
-        if (columns.Count <= i)
-            columns.Add(new Column());
-
-        if (lines[i] == ' ')
+        if (line[i] == ' ')
+        {
+            columns[columnIndex].Values.Add(newValue);
+            newValue = 0;
             columnIndex++;
+        }
 
         int digit = line[i] - '0';
         newValue *= 10;
         newValue += digit;
     }
-    columns[columnIndex].Values.Add(newValue);
-    newValue = 0;
+
 }
 
-Console.WriteLine($"Result: {columns.Sum(c => c.Calculate(c.Operation))}");
+for (int i = 0; i < columns.Count; i++)
+{
+    columns[i].Calculate(operations[i]);
+}
+
+Console.WriteLine($"Result: {columns.Sum(c => c.Result)}");
 
 public class Column
 {
