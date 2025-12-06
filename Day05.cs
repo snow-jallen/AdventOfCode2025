40ddcc0d5eec665a062fa94a20a14829304a5498
Jonathan Allen <jallen@snow.edu>
2025-12-05 19:43:11 -0700

    day5 part 2 more progress

--- Day05.cs
+++ Day05.cs
@@ -3,24 +3,60 @@ string path = args.FirstOrDefault() == "real" ? "real.txt" : "sample.txt";
 //var lines = File.ReadAllLines(path);
 var lines = File.ReadAllLines(path).Where(l => string.IsNullOrWhiteSpace(l) is false).ToList();
 var ranges = from line in lines
-        where line.Contains("-")
-        let parts = line.Split('-')
-        select (Start: long.Parse(parts[0]), End: long.Parse(parts[1]));
+             where line.Contains("-")
+             let parts = line.Split('-')
+             select (Start: long.Parse(parts[0]), End: long.Parse(parts[1]));
 
 var spoiled = from line in lines
-        where line.Contains("-") is false
-        let ingredientId = long.Parse(line)
-        where ranges.Any(r => ingredientId >= r.Start && ingredientId <= r.End)
-        select ingredientId;
+              where line.Contains("-") is false
+              let ingredientId = long.Parse(line)
+              where ranges.Any(r => ingredientId >= r.Start && ingredientId <= r.End)
+              select ingredientId;
 
 Console.WriteLine($"Total Spoiled Ingredients: {spoiled.Count()}");
 
-long freshIngredientCount = 0;
-long lastStart = -1;
-List<(long Start, long End)> processedRanges = new ();
-foreach(var range in ranges)
+List<(long Start, long End)> processedRanges = new();
+foreach (var range in ranges)
 {
-    if(processedRanges.an)
+    //early range
+    var firstProcessedRange = processedRanges.OrderBy(p => p.Start).FirstOrDefault();
+    if(firstProcessedRange == default || range.End < firstProcessedRange.Start)
+    {
+        processedRanges.Add(range);
+        continue;
+    }
+
+    //early overlap
+    var earlyOverlap = processedRanges.FirstOrDefault(p => range.End >= p.Start && range.End <= p.End);
+    if(earlyOverlap != default)
+    {
+        if(range.Start < earlyOverlap.Start)
+        {
+            processedRanges.Remove(earlyOverlap);
+            processedRanges.Add((range.Start, earlyOverlap.End));
+            continue;
+        }
+    }
+
+    // Merge overlapping ranges
+    var overlapping = processedRanges.FirstOrDefault(p => range.Start >= p.Start && range.Start <= p.End);
+    if (overlapping != default)
+    {
+        if (range.End > overlapping.End)
+        {
+            processedRanges.Remove(overlapping);
+            processedRanges.Add((overlapping.Start, range.End));
+            continue;
+        }
+    }
+    else
+    {
+        processedRanges.Add(range);
+        continue;
+    }
+
+    Console.WriteLine($"Unhandled range case: {range}");
 }
 
-Console.WriteLine($"Total Fresh Ingredients: {freshIngredients.Count}");
+var sum = processedRanges.Sum(r => r.End - r.Start + 1);
+Console.WriteLine($"Total Fresh Ingredients: {sum}");
